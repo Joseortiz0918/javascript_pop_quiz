aff04e51a39119d1a712dad4bcdaf70790bee16c
Joseortiz0918 <145424169+Joseortiz0918@users.noreply.github.com>
2023-11-18 00:01:56 -0600

    added outline in js to start off my functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,36 @@
-//need begin button
+//when start is clicked 
+//timer is activated 
+//1st question propagates 
+//4 multiple choices are propagated 
+
+// when right answer is clicked 
+//2nd question and choices are propagated
+//a message will display 'correct'
+
+//when wrong choice is clicked 
+//timer will be reduced by 3 seconds 
+//a message will display 'Wrong choice'
+//next question and answers will display
+
+//when quiz is done
+// you will presented with your score 
+//and a form to save your initials and score 
+//a clear button will show up and allow you to clear history
+
+
+
+
+// timer
+    
+// in html timer has id timer 
+
+
+
+
+//when see scores is clicked takes us to a new webpage that has the stored values of the previous last high scores
+// need a way to be able to store data locally
+// of hs and user name
+//need a way to order scores from high to low 
+
+
+
